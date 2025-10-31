348a2e35eabf354f9f7bcfdbcc3c24ad83b67234
GPS4444 <simcraft@outlook.it>
2025-10-31 12:12:25 +0100

    Challenge 2 plat

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    [SerializeField] Vector3 dogSpawnPosition = new Vector3(0, 0, 0);
+    [SerializeField] float cooldown = 2.0f;
+
+    private float timer = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (timer > cooldown)
+            {
+                Instantiate(dogPrefab, dogSpawnPosition, dogPrefab.transform.rotation);
+                timer = 0;
+            }
+        }
+        timer += Time.deltaTime;
+    }
+}
---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    [Header("Spawn Position")]
+    [SerializeField] float spawnLimitXLeft = -22;
+    [SerializeField] float spawnLimitXRight = 7;
+    [SerializeField] float spawnPosY = 30;
+
+    [Header("Time")]
+    [SerializeField] float startDelay = 1.0f;
+    [SerializeField] float spawnIntervalStart = 3.0f;
+    [SerializeField] float spawnIntervalEnd = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(nameof(SpawnRandomBall), startDelay, Random.Range(spawnIntervalStart, spawnIntervalEnd));
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+    }
+
+}
